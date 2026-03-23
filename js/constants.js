863d62a1f60c2f7c41a28144cb5d34fcb2234b74
adrzcn <72085355+adcousin@users.noreply.github.com>
2026-03-23 12:01:57 +0100

    Feat/geo coordinates filters (#38)
    * feat(filter) remove aven filters and implement geo-coordinates filters with "très proche" "proche" "moins proche"
    
    * feature - implement Ꝃ and deuil électoral
    
    * feature - implement more deuil électoral

--- js/constants.js
+++ js/constants.js
@@ -5,7 +5,20 @@ const PUBLIC_KEY = "886db2f2acd749bc9a4a3eeda9a6d0dc";
 const URL = `https://api.openagenda.com/v2/agendas/${AGENDA_UID}/events`;
 const maxEvents = 400;
 
-const AREA_FILTERS = ["AVEN", "CORNOUAILLE", "BRETAGNE"];
+// geographic area filters are no longer used; distance categories are calculated dynamically
+
+// reference point used for distance calculations (DD coordinates)
+const REFERENCE_COORDS = { // CONCARNEAU
+    latitude: 47.87502,
+    longitude: -3.92245
+};
+
+// thresholds in kilometres for the three distance categories
+const DISTANCE_THRESHOLDS = {
+    TRES_PROCHE: 5,   // <5 km
+    PROCHE: 25        // <25 km (MOINS PROCHE otherwise)
+};
+
 
 const defaultParams = {
     // relative: ['current', 'upcoming'], // Do not use this, OpenAgenda params are not standard
@@ -16,7 +29,7 @@ const defaultParams = {
     monolingual: "fr",
 };
 
-const includeFields = ["uid","slug", "title", "onlineAccessLink","registration", "status", "keywords", "dateRange", "location.description", "nextTiming", "longDescription", "description", "location.name", "location.city", "keywords"]
+const includeFields = ["uid","slug", "title", "onlineAccessLink","registration", "status", "keywords", "dateRange", "location.description", "nextTiming", "longDescription", "description", "location.name", "location.city", "location.latitude", "location.longitude", "keywords"]
 
 const defaultQuery = URL + "?" 
 + new URLSearchParams(defaultParams).toString()
